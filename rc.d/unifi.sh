94191f1e75ccc4555e23644c6179267a15e8e258
envescent <agc@envescent.com>
2024-04-14 14:14:41 -0400

    Update unifi.sh
    Running unifi.sh service as unifi user rather than root to improve security. May break things. Working on testing further.

--- rc.d/unifi.sh
+++ rc.d/unifi.sh
@@ -26,7 +26,7 @@ unifi_start()
 
     # The process will run until it is terminated and does not fork on its own.
     # So we start it in the background and stash the pid:
-    /usr/local/bin/java --add-opens=java.base/java.time=ALL-UNNAMED -jar /usr/local/UniFi/lib/ace.jar start &
+    su unifi -c "/usr/local/bin/java --add-opens=java.base/java.time=ALL-UNNAMED -jar /usr/local/UniFi/lib/ace.jar start" &
     echo $! > $pidfile
 
   fi
@@ -40,7 +40,7 @@ unifi_stop()
 
     # This process does take a while, but the stop command finishes before
     # the service is actually stopped. So we start it in the background:
-    /usr/local/bin/java -jar /usr/local/UniFi/lib/ace.jar stop &
+    su unifi -c "/usr/local/bin/java -jar /usr/local/UniFi/lib/ace.jar stop" &
 
     # Get the pid of the stopper:
     stopper=$!
